66321370f7cd512f58a97ac85eae7ee2ff107d89
jugalkpatel <ptjimmy99@gmail.com>
2021-04-14 00:11:58 +0530

    fix: stop scroll on modal open

--- components/modal.js
+++ components/modal.js
@@ -1,8 +1,12 @@
 function showModal() {
     let modal = document.getElementById("modal");
+    let body = document.getElementById("body");
+    console.log(body);
+    body.classList.add("stop--scroll");
     modal.classList.toggle('modal--show');
 }
 
 function closeModal() {
+    body.classList.remove("stop--scroll");
     modal.classList.remove("modal--show");
 }
